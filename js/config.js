7f5c39556f2c00a3b531d9b7e76b1a46d157a609
Hardik Rangucha <111430903+hardikrangucha@users.noreply.github.com>
2025-08-21 18:19:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_vh74v1cOg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1qe8kk7ed1t2mdud1nlfkq1hk3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2dscpd5a0fln9rve4034t59maf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
